e4dbb3053ff20e4116ec47f07688bb0f065c7bdc
saifhalila <saifhalila@gmail.com>
2018-02-09 16:56:10 +0700

    Set up the methods for Team A
    Add displayForTeamA to update the score

--- app/src/main/java/io/github/saifhalila/courtcounter/MainActivity.java
+++ app/src/main/java/io/github/saifhalila/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package io.github.saifhalila.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreePoints(View view) {
+        displayForTeamA(3);
+    }
+
+    public void addTwoPoints(View view) {
+        displayForTeamA(2);
+    }
+
+    public void addFreeThrow(View view) {
+        displayForTeamA(1);
     }
 }
