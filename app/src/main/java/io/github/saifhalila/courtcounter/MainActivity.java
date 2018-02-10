ab08bc5364c8f2718ea384c3d6cb338f2dd6043a
saifhalila <saifhalila@gmail.com>
2018-02-10 10:47:50 +0700

    Displays the given score for Team B.
    *Add displayForTeamB()
    
    *Add the three methods to update the score.

--- app/src/main/java/io/github/saifhalila/courtcounter/MainActivity.java
+++ app/src/main/java/io/github/saifhalila/courtcounter/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
+    int scoreTeamB = 0 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score_A);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -35,5 +36,26 @@ public class MainActivity extends AppCompatActivity {
     public void addOneForTeamA(View view) {
         scoreTeamA+=1;
         displayForTeamA(scoreTeamA);
+    }
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score_B);
+        scoreView.setText(String.valueOf(score));
+    }
+    public void addThreeForTeamB(View view) {
+        scoreTeamB+=3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoForTeamB(View view) {
+        scoreTeamB+=2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addOneForTeamB(View view) {
+        scoreTeamB+=1;
+        displayForTeamB(scoreTeamB);
     }
 }
